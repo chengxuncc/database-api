ada14781e1e5b6bce4fc96b84543a895e6aa4aa9
moyuxii <2543731862@qq.com>
2019-03-11 14:09:08 +0800

    更改了查询，改为较为通用的查询

--- mm.go
+++ mm.go
@@ -62,11 +62,12 @@ func Insert(db *sql.DB, obj interface{}) error {
 	return err
 }
 
+
 func main() {
 	db, err := sql.Open("postgres",     /*连接数据库*/
 		fmt.Sprintf(
 			"host=%s port=%s user=postgres dbname=%s password=%s sslmode=disable",
-			HOST,PORT,DBNAME,PASSWORD,
+			"127.0.0.1","5432","postgres","312030",
 		),
 	)
 	if err != nil {    /*获取连接错误*/
@@ -74,25 +75,21 @@ func main() {
 	}
 	router := gin.Default()
 
-	router.GET("/Goods_query", func(c *gin.Context) {   /*按名字或条形码查询*/
+	router.GET("/easy_select", func(c *gin.Context) {
 		var goods []Goods  /*切片*/
 		var good  Goods
-		name := c.Query("name")
-		barcode_id :=c.Query("barcode_id")
+		table_name := c.Query("table_name")    /*接收查询的表*/
+		Qualif := c.Query("qualif")     /*接收查询条件*/
 		var query string
-		if name != ""{
-			query = "select * from goods where name = '"+name+"'"
+		if Qualif != ""{
+			query = "select * from "+table_name+" where "+Qualif
 		}
-		if barcode_id != ""{
-			query = "select * from goods where barcode_id = '"+barcode_id+"'"
-		}
-		if name == ""&&barcode_id == ""{
-			query = "select * from goods"
+		if Qualif == ""{
+			query = "select * from "+table_name
 		}
 		Rows,err:= db.Query(query)
 		if err ==nil{
 			if Rows != nil{
-				fmt.Println("OK")
 				for Rows.Next(){
 					err  =Rows.Scan(&good.Id,&good.Name,&good.Num,&good.Im_price,&good.Barcode_id,&good.Discount,&good.Sup_id)
 					goods = append(goods, good)   /*切片增加*/
@@ -106,6 +103,39 @@ func main() {
 		}
 		c.JSON(200,Response{"error",err.Error()})
 	})
+
+	//router.GET("/Goods_query", func(c *gin.Context) {   /*按名字或条形码查询*/
+	//	var goods []Goods  /*切片*/
+	//	var good  Goods
+	//	name := c.Query("name")      /*返回的string类型*/
+	//	barcode_id :=c.Query("barcode_id")
+	//	var query string
+	//	if name != ""{
+	//		query = "select * from goods where name = '"+name+"'"
+	//	}
+	//	if barcode_id != ""{
+	//		query = "select * from goods where barcode_id = "+barcode_id
+	//	}
+	//	if name == ""&&barcode_id == ""{
+	//		query = "select * from goods"
+	//	}
+	//	fmt.Println(query)
+	//	Rows,err:= db.Query(query)
+	//	if err ==nil{
+	//		if Rows != nil{
+	//			for Rows.Next(){
+	//				err  =Rows.Scan(&good.Id,&good.Name,&good.Num,&good.Im_price,&good.Barcode_id,&good.Discount,&good.Sup_id)
+	//				goods = append(goods, good)   /*切片增加*/
+	//			}
+	//			Rows.Close()
+	//			if err == nil{
+	//				c.JSON(200,goods)
+	//				return
+	//			}
+	//		}
+	//	}
+	//	c.JSON(200,Response{"error",err.Error()})
+	//})
 
 	router.POST("/supplier", func(c *gin.Context) {
 
